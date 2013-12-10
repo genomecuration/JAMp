7fc89d70d2e35aedcaaf6f45d1c0b5c1ecb449bf
Alexie <alpapan@326392.no-reply.drupal.org>
2013-12-11 10:11:56 +1100

    licensing

--- annotate/process_ipr.pl
+++ annotate/process_ipr.pl
@@ -1,5 +1,26 @@
 #!/usr/bin/perl
 
+=pod
+
+
+=head1 AUTHORS
+
+ Alexie Papanicolaou
+
+        CSIRO Ecosystem Sciences
+        alexie@butterflybase.org
+
+=head1 DISCLAIMER & LICENSE
+
+Copyright 2012-2014 the Commonwealth Scientific and Industrial Research Organization. 
+This software is released under the Mozilla Public License v.2.
+
+It is provided "as is" without warranty of any kind.
+You can find the terms and conditions at http://www.mozilla.org/MPL/2.0.
+
+
+=cut
+
 use strict;
 use warnings;
 use XML::LibXML::Reader;
--- util/hhsearch_processing/ffindex_gather.sh
+++ util/hhsearch_processing/ffindex_gather.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# if you don't specify an argument file then it will look for *.all.db outputs and strip the 0 delimiting character
-# to make a proper text file output
+#e.g $1 = transposon_db.nr90.db
 
 if [ $1 ]; then
-        if [ -e $1.db ]; then
-	   ffindex_build -as $1.all.db $1.all.idx -d $1.db -i $1.db.idx
-	   rm -f $1.db $1.db.idx
+        if [ -e "$1"_out2.db ]; then
+	   ffindex_build -as "$1"_out2.all.db "$1"_out2.all.idx -d "$1"_out2.db -i "$1"_out2.db.idx
+	   rm -f "$1"_out2.db "$1"_out2.db.idx
 	fi
 	for i in {0..100}; \
          do 
-          if [ -e $1.db.$i ]; then 
-	   ffindex_build -as $1.all.db $1.all.idx -d $1.db.$i -i $1.db.idx.$i
-           rm -f $1.db.$i $1.db.idx.$i
+          if [ -e "$1"_out2.db.$i ]; then 
+	   ffindex_build -as "$1"_out2.all.db "$1"_out2.all.idx -d "$1"_out2.db.$i -i "$1"_out2.db.idx.$i
+           rm -f "$1"_out2.db.$i "$1"_out2.db.idx.$i
           fi
 	 done
-        ffindex_resume.pl *orig *all.idx
+        ffindex_resume.pl "$1".idx.orig "$1"_out2.all.idx
 else
     cat  *.all.db |tr -d '\000' >  results.all.db.hhr
 fi
